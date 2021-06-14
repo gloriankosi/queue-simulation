2f7bb39dcd29618478eed9862bde9ef67c7f1cee
gloriankosi <kosig@acm.org>
2021-06-14 18:08:42 -0400

    doc and starter code

---
+++ queue-simulation.cc
@@ -0,0 +1,15 @@
+/**
+ * @file queue-simulation.cc
+ * @author Glorian Kosi
+ * @brief 
+ * 
+ * 
+ */
+#include <iostream>
+#include <iterator>
+#include <random>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
