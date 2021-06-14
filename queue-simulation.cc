41b05131c0c961da22490f8a625cd0bec30e4727
gloriankosi <kosig@acm.org>
2021-06-14 19:42:29 -0400

    added types and classes

--- queue-simulation.cc
+++ queue-simulation.cc
@@ -3,13 +3,43 @@
  * @author Glorian Kosi
  * @brief 
  * 
+ * See readme
  * 
  */
 #include <iostream>
 #include <iterator>
 #include <random>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::vector;
+
+typedef struct
+{
+    int hour;
+    int min;
+    int sec;
+} Clock;
+
+typedef class ServiceStation
+{
+
+} ServiceStation;
+
+typedef class
+{
+    long int passId;
+    int serviceType;
+    Clock arrivalTime; // Arrival to queue
+    Clock serviceTime; // Time elapsed to enter service station
+} PassengerType;
 
 int main(int argc, char *argv[])
 {
+    double checkInDuration;
+    cout << "Duration of check-in (in minutes): ";
+    cin >> checkInDuration;
+
     return 0;
 }
