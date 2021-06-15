c5f5e66fc2995156fa81d976e9d16fc4e23219fb
gloriankosi <kosig@acm.org>
2021-06-15 18:10:35 -0400

    added comments, adjusted the members of Queue, added a Node struct

--- queue-simulation.cc
+++ queue-simulation.cc
@@ -21,6 +21,7 @@
 using std::ceil;
 using std::cin;
 using std::cout;
+using std::endl;
 using std::mt19937;
 using std::normal_distribution;
 using std::random_device;
@@ -31,30 +32,32 @@ struct Clock;
 class Passenger;
 class ServiceStation;
 struct Queue;
+struct Node;
 
 struct Clock
 {
     int hour;
     int min;
 };
-
-class Passenger
+struct Passenger
 {
     long int pasId;
     int serviceType;
     Clock arrivalTime; // Arrival to queue
     Clock serviceTime; // Time elapsed to enter service station
 };
-
 struct Queue
 {
     long maxSize;
     long currentSize;
-    Passenger pas;
-    Queue *next;
+    Node *root;
 };
-
-class ServiceStation
+struct Node
+{
+    Passenger pas;
+    Node *next;
+};
+struct ServiceStation
 {
     Passenger currentPas;
     Clock remainingServiceTime;
@@ -63,14 +66,27 @@ class ServiceStation
 
 int main(int argc, char *argv[])
 {
+    /**
+     * @brief 
+     * Passenger IDs are incremented by 1 iteratively, currentPasNumber keeps track of the latest reserved
+     * long int, the next passenger yet to arrive in line is currentPasNumber + 1, the second is currentPasNumber + 2, and so on.
+     * 
+     * Queues are not guaranteed to have Passenger IDs in numerical order (nor should it happen, if it does, there
+     * is something wrong with the prng)
+     * 
+     */
+    long int currentPasNumber = 0;
+
     random_device rd{};
     mt19937 gen{rd()};
 
     uniform_int_distribution<int> hourUd(0, 23), minUd(0, 59);
+    uniform_int_distribution<int> serviceClassUd(0, 9);
 
-    Clock worldClock{hourUd(rd), minUd(rd)};
+    Clock worldClock{hourUd(rd), minUd(rd)}, elapsedTime;
 
     Queue coachQueue, firstClassQueue;
+    Node *coachRoot, *firstClassRoot = new Node(); // Root nodes of Queue (first passenger in line occupate this node)
 
     ServiceStation coachServiceStation1, coachServiceStation2, coachServiceStation3;
     ServiceStation firstClassServiceStation1, firstClassServiceStation2, firstClassServiceStation3;
@@ -84,25 +100,27 @@ int main(int argc, char *argv[])
     long firstPasClassAvgServiceDuration;
 
     cout << "Please enter an unsigned integer value, this will be "
-         << "used as the stopping point for the simulation, this value represents the number of\n"
-         << "minutes elapsed in the simulation " << MAGENTA << "not real time." << RESET << '\n';
+         << "used as the stopping point for the simulation, all values represents the number of "
+         << "minutes elapsed in the simulation, which goes by iterations in a loop," << MAGENTA << " not real time." << RESET;
+    cout << '\n';
+
     cout << "Duration time: ";
     cin >> checkInDuration;
 
-    cout << "Average passenger arrival rate of COACH station (in minutes): ";
+    cout << "Average passenger arrival rate of COACH station: ";
     cin >> coachPasAvgArrivalRate;
     normal_distribution<double> coachPasAvgArrivalRateNd(coachPasAvgArrivalRate, 1);
 
-    cout << "Average passenger service duration of COACH station (in minutes): ";
+    cout << "Average passenger service duration of COACH station: ";
     cin >> coachPasAvgServiceDuration;
     // Draw a new value that denotes how long the Passenger actually took, use ceil to bring it to the nearest min
     normal_distribution<double> coachPasAvgServiceDurationNd(coachPasAvgServiceDuration, 1);
 
-    cout << "Average passenger arrival rate of FIRST CLASS station (in minutes): ";
+    cout << "Average passenger arrival rate of FIRST CLASS station: ";
     cin >> firstPasClassAvgArrivalRate;
     normal_distribution<double> firstPasClassAvgArrivalRateNd(firstPasClassAvgArrivalRate, 1);
 
-    cout << "Average passenger service duration of FIRST CLASS station (in minutes): ";
+    cout << "Average passenger service duration of FIRST CLASS station: ";
     cin >> firstPasClassAvgServiceDuration;
     normal_distribution<double> firstPasClassAvgServiceDurationNd(firstPasClassAvgServiceDuration, 1);
 
