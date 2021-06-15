0e0895fdb48e382c79b62faacad2b580f5c885af
gloriankosi <kosig@acm.org>
2021-06-15 01:43:40 -0400

    output and more needed classes and variables

--- queue-simulation.cc
+++ queue-simulation.cc
@@ -6,40 +6,97 @@
  * See readme
  * 
  */
+
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define YELLOW "\033[33m"
+#define MAGENTA "\033[35m"
+
 #include <iostream>
 #include <iterator>
 #include <random>
 #include <vector>
+#include <cmath>
 
+using std::ceil;
 using std::cin;
 using std::cout;
+using std::mt19937;
+using std::normal_distribution;
+using std::random_device;
 using std::vector;
 
-typedef struct
+struct Clock;
+class Passenger;
+class ServiceStation;
+struct Queue;
+
+struct Clock
 {
     int hour;
     int min;
-    int sec;
-} Clock;
+};
 
-typedef class ServiceStation
+class Passenger
 {
-
-} ServiceStation;
-
-typedef class
-{
-    long int passId;
+    long int pasId;
     int serviceType;
     Clock arrivalTime; // Arrival to queue
     Clock serviceTime; // Time elapsed to enter service station
-} PassengerType;
+};
+
+struct Queue
+{
+    long maxSize;
+    long currentSize;
+    Passenger pas;
+    Queue *next;
+};
+
+class ServiceStation
+{
+    Passenger currentPas;
+    Clock remainingServiceTime;
+    Clock arrivalTime;
+};
 
 int main(int argc, char *argv[])
 {
-    double checkInDuration;
-    cout << "Duration of check-in (in minutes): ";
+    random_device rd{};
+    mt19937 gen{rd()};
+
+    Queue coachQueue;
+    Queue firstClassQueue;
+
+    ServiceStation coachServiceStation1;
+    ServiceStation coachServiceStation2;
+    ServiceStation coachServiceStation3;
+
+    ServiceStation firstClassServiceStation1;
+    ServiceStation firstClassServiceStation2;
+    ServiceStation firstClassServiceStation3;
+
+    long checkInDuration; // Total duration of simulation
+
+    long coachPasAvgArrivalRate;
+    long coachPasAvgServiceRate; // Average time it takes to service the next customer in queue
+
+    long firstPasClassAvgArrivalRate;
+    long firstPasClassAvgServiceRate;
+
+    cout << "Please enter an unsigned integer value, this will be "
+         << "used as the stopping point for the simulation, this value represents the number of\n"
+         << "minutes elapsed in the simulation " << MAGENTA << "not real time." << RESET << '\n';
+    cout << "Duration time: ";
     cin >> checkInDuration;
+
+    cout << "Average passenger arrival rate of COACH station (in minutes): ";
+    cin >> coachPasAvgArrivalRate;
+
+    cout << "Average passenger service duration of COACH station (in minutes): ";
+    cin >> coachPasAvgServiceRate;
+    // Draw a new value that denotes how long the Passenger actually took, use ceil to bring it to the nearest min
+    normal_distribution<double> coachPasAvgServiceRateNd(coachPasAvgServiceRate, 1);
 
     return 0;
 }
