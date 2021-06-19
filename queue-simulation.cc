e48d56a05531ffc54b1c24975d72c9488687707f
gloriankosi <kosig@acm.org>
2021-06-19 00:26:30 -0400

    variable unpacking at start, new stacks to track available stations

--- queue-simulation.cc
+++ queue-simulation.cc
@@ -1,9 +1,13 @@
 /**
+ * 
  * @file queue-simulation.cc
  * @author Glorian Kosi
  * @brief 
  * 
  * See readme
+ * To run:
+ * g++ -std=c++2a queue-simulation.cc -o executable_name
+ * ./executable_name
  * 
  */
 
@@ -25,9 +29,11 @@ using std::endl;
 using std::mt19937;
 using std::normal_distribution;
 using std::random_device;
+using std::reference_wrapper;
 using std::uniform_int_distribution;
 using std::vector;
 
+/********************* Class declarations ***************************/
 struct Clock;
 struct Passenger;
 struct ServiceStation;
@@ -37,8 +43,11 @@ struct Parameters;
 struct ServiceDuration;
 struct Arrival;
 
-void start(Parameters &);
+/********************* Function declarations *************************/
+void start(Parameters &, vector<ServiceStation> &);
+void initialQueueFill(Parameters &);
 
+/********************* Class definitions ***************************/
 struct ArrivalRate
 {
     long coachPasAvgArrivalRate;
@@ -52,13 +61,17 @@ struct ServiceDuration
 struct Clock
 {
     int min;
+    Clock() : min(0){};
+    Clock(long a) : min(a){};
 };
 struct Passenger
 {
     long int pasId;
     int serviceType;
     Clock arrivalTime; // Arrival to queue
-    Clock serviceTime; // Time elapsed to enter service station
+    Clock serviceTime; // Time needed for service
+    Passenger() : pasId(-1), serviceType(-1){};
+    Passenger(long a) : pasId(a){};
 };
 struct Queue
 {
@@ -68,6 +81,8 @@ struct Queue
     Node *back;
     void enqueue(Clock &, long int &);
     void dequeue(Clock &);
+
+    Queue() : maxSize(0), currentSize(0){};
 };
 struct Node
 {
@@ -76,40 +91,39 @@ struct Node
 };
 struct ServiceStation
 {
-    Passenger currentPas;
-    Clock remainingServiceTime;
+    long totalTimeInUse; // Divide by total simulation time to get percentage
+    int type;
+    Passenger *currentPas;
     Clock arrivalTime;
+    Clock remainingServiceTime;
+    ServiceStation(int a) : type(a), currentPas(nullptr){};
 };
 struct Dist
 {
     /**
      * @brief 
      * getFromUniformDist generates a random integer from a uniform distribution, in this
-     * simulation, even numbers denote coach service, and odd numbers denote first class service
+     * simulation, even numbers denote coach service, and odd numbers denote first class service.
      */
     static long int getFromUniformDist(long, long);
     static long int getFromNormalDist(long, long);
 };
+/**
+ * @brief 
+ * Contains closely related variables needed in most cases that can't be combined into more classes
+ * or can't be removed without the use of global variables.
+ */
 struct Parameters
 {
     long simuDuration; // Total duration of simulation
-    long int currentPasNumber;
+    long currentPasNumber;
 
+    Queue *firstClassQueue;
+    Queue *coachQueue;
     ServiceDuration serviceDuration;
     ArrivalRate arrivalRate;
     Clock worldClock, elapsedTime;
-};
-
-void start(Parameters &P)
-{
-    /**
-     * @brief 
-     * Naming aliases
-     */
-    auto &coachA = P.arrivalRate.coachPasAvgArrivalRate;      // alias for coachPasAvgArrivalRate from Arrival struct
-    auto &firstA = P.arrivalRate.firstClassPasAvgArrivalRate; //alias for firstClassPasAvgArrivalRate from Arrival struct
-
-    // params.getFromNormalDist(params.arrivalRate.coachPasAvgArrivalRate, 1);
+    Parameters() : simuDuration(0), currentPasNumber(0){};
 };
 
 // TODO: Split main into a function Parameters initParams() for code cleanliness, not needed right now, but needed for later
@@ -127,14 +141,14 @@ int main(int argc, char *argv[])
 
     Parameters params;
 
-    params.currentPasNumber = 0;
-    params.worldClock = {0}, params.elapsedTime = {0};
-
     Queue coachQueue, firstClassQueue;
     Node *coachRoot, *firstClassRoot = new Node(); // Root nodes of Queue (first passenger in line occupate this node)
 
-    ServiceStation coachServiceStation1, coachServiceStation2, coachServiceStation3;
-    ServiceStation firstClassServiceStation1, firstClassServiceStation2, firstClassServiceStation3;
+    ServiceStation coachServiceStation1(0), coachServiceStation2(0), coachServiceStation3(0), coachServiceStation4(0);
+    ServiceStation firstClassServiceStation1(1), firstClassServiceStation2(1), firstClassServiceStation3(1);
+
+    vector<ServiceStation> serviceStations{coachServiceStation1, coachServiceStation2, coachServiceStation3, coachServiceStation4,
+                                           firstClassServiceStation1, firstClassServiceStation2, firstClassServiceStation3};
 
     ServiceDuration serviceDuration;
     ArrivalRate arrivalRate;
@@ -147,13 +161,13 @@ int main(int argc, char *argv[])
     cout << "Duration time: ";
     cin >> params.simuDuration;
 
-    cout << "Average passenger arrival rate of COACH station: ";
+    cout << "Average passenger arrival rate of COACH station (for best measure, enter value significantly less than duration time): ";
     cin >> arrivalRate.coachPasAvgArrivalRate;
 
     cout << "Average passenger service duration of COACH station: ";
     cin >> serviceDuration.coachPasAvgServiceDuration;
 
-    cout << "Average passenger arrival rate of FIRST CLASS station: ";
+    cout << "Average passenger arrival rate of FIRST CLASS station (for best measure, enter value significantly less than duration time): ";
     cin >> arrivalRate.firstClassPasAvgArrivalRate;
 
     cout << "Average passenger service duration of FIRST CLASS station: ";
@@ -183,13 +197,68 @@ int main(int argc, char *argv[])
         firstClassQueue.front = firstClassRoot;
         firstClassQueue.back = firstClassRoot;
     }
+    params.firstClassQueue = &firstClassQueue;
+    params.coachQueue = &coachQueue;
 
     cout << "Starting..." << endl;
 
-    start(params);
+    start(params, serviceStations);
 
     return 0;
 }
+
+/********** Function Definitions *************/
+void start(Parameters &P, vector<ServiceStation> &serviceStations)
+{
+    /**
+     * @brief 
+     * Naming aliases and variable unpacking
+     */
+    auto &coachA = P.arrivalRate.coachPasAvgArrivalRate;      // alias for coachPasAvgArrivalRate from Arrival struct
+    auto &firstA = P.arrivalRate.firstClassPasAvgArrivalRate; //alias for firstClassPasAvgArrivalRate from Arrival struct
+
+    auto &simuDuration = P.simuDuration;
+    auto &currentPasNumber = P.currentPasNumber;
+
+    auto &firstClassQueue = P.firstClassQueue;
+    auto &coachQueue = P.coachQueue;
+
+    auto &currentTime = P.worldClock.min;
+
+    auto nextCoachPasArrivalTime = Dist::getFromNormalDist(coachA, 1);
+    auto nextFirstClassPasArrivalTime = Dist::getFromNormalDist(firstA, 1);
+
+    vector<ServiceStation *> availableCoachStations;
+    vector<ServiceStation *> availableFirstClassStations;
+
+    auto checkAvailable = [&availableCoachStations, &availableFirstClassStations](vector<ServiceStation> &serviceStations)
+    {
+        for (auto &station : serviceStations)
+        {
+            if (station.currentPas == nullptr)
+            {
+                if (station.type == 0)
+                {
+                    availableCoachStations.emplace_back(&station);
+                }
+                else
+                {
+                    availableFirstClassStations.emplace_back(&station);
+                }
+            }
+        }
+    };
+
+    checkAvailable(serviceStations);
+
+    for (auto i = currentTime; i <= simuDuration; i++)
+    {
+        if (currentTime > nextCoachPasArrivalTime)
+        {
+        }
+    }
+};
+
 void Queue::enqueue(Clock &worldClock, long int &currentPasNumber)
 {
     auto *newPas = new Node();
